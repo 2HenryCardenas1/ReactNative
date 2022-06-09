f4c0a69090ff3c226ec6227009effb2f8d462338
ivan886 <ivanfernando.fonseca@gmail.com>
2022-06-08 22:39:42 -0500

    mejorando aspecto visual header home

--- TravelApp/TravelApp/src/components/Home/Header.js
+++ TravelApp/TravelApp/src/components/Home/Header.js
@@ -4,15 +4,20 @@ import { capitalize } from "lodash";
 
 export default function Header(props) {
     const { image } = props;
-   
+
     return (
         <>
             <View style={styles.container}>
-                <View>
-                <View><Text>user</Text></View>
-                <View><Text>icono </Text></View>
+                <View style={styles.containerOptions}>
+                    <View style={styles.containerOptions}>
+                        <View style={styles.perfil} ></View>
+                        <Text>user</Text>
+                    </View>
+                    <Text >icono </Text>
                 </View>
-                <Text>¿A cuál lugar deseas ir?</Text>
+
+
+                <Text style={styles.textoLugar}>¿A cuál lugar deseas ir?</Text>
             </View>
 
         </>
@@ -22,7 +27,30 @@ export default function Header(props) {
 const styles = StyleSheet.create({
     container: {
         flex: 1,
+        marginLeft: "auto",
+        marginRight: "auto",
+        width: "90%",
+
     },
+    textoLugar: {
+        fontSize: 38,
+
+    },
+    perfil: {
+        borderColor: "#dbdbce",
+        borderWidth: 2,
+        height: 40,
+        width: 40,
+        borderRadius: "90",
+        backgroundColor: "#fe9345",
     
-   
+    },
+    containerOptions: {
+        flex: 1,
+        flexDirection: "row",
+        alignItems:"center",
+        gap:"2"
+    }
+
+
 });
